387af4233536855e20f8b0283e3db332499bbb92
LuisM000 <luis.m.r@outlook.com>
2019-12-27 18:03:48 +0100

    Added some tests ...

--- Mosqueton/Game.cs
+++ Mosqueton/Game.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
 using Mosqueton.Data;
 using Mosqueton.IServices;
 
@@ -21,20 +22,24 @@ namespace Mosqueton
             _levelManager = levelManager;
 
             Content.RootDirectory = "GameContent";
-            _graphics = new GraphicsDeviceManager(this);
-            _graphics.SupportedOrientations = DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight;
+            _graphics = new GraphicsDeviceManager(this)
+            {
+                SupportedOrientations = DisplayOrientation.LandscapeLeft | DisplayOrientation.LandscapeRight
+            };
         }
 
         protected override void Initialize()
         {
-            
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-           
+            using (var stream = TitleContainer.OpenStream("GameContent/Background.jpg"))
+            {
+                _scenario = Texture2D.FromStream(_graphics.GraphicsDevice, stream);
+            }
             base.LoadContent();
         }
 
@@ -43,12 +48,29 @@ namespace Mosqueton
         {
             _levelManager.Update(gameTime.ElapsedGameTime);
 
+            TouchCollection touchCollection = TouchPanel.GetState();
+
+            if (touchCollection.Count > 0)
+            {
+                var touch = touchCollection[0].Position;
+                var matrix = Matrix.Invert(Matrix.CreateScale((float)_graphics.GraphicsDevice.Viewport.Width / 1920,
+                           (float)_graphics.GraphicsDevice.Viewport.Height / 1080,
+                           1f));
+                
+                var touchTransformed = Vector2.Transform(touch, matrix);
+                System.Diagnostics.Debug.WriteLine(touchTransformed);
+
+            }
+
             base.Update(gameTime);            
         }
 
         protected override void Draw(GameTime gameTime)
 		{
-            _spriteBatch.Begin();
+            var matrix = Matrix.CreateScale((float)_graphics.GraphicsDevice.Viewport.Width / 1920,
+                           (float)_graphics.GraphicsDevice.Viewport.Height / 1080,
+                           1f);
+            _spriteBatch.Begin(transformMatrix: matrix);
             _spriteBatch.Draw(_scenario, Vector2.Zero, Color.White);
             _spriteBatch.End();
 
